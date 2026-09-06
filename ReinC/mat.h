0f15633a2578691b17c7e6c024af7256e7465160
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-07 00:17:46 +0530

    small changes

--- ReinC/mat.h
+++ ReinC/mat.h
@@ -6,7 +6,7 @@
 
 typedef struct {
   u32 rows, cols;
-  f32 *data; // row-major
+  f32 *data; // row major
 } matrix;
 
 matrix *create_matrix(mem_arena *arena, u32 rows, u32 cols);
@@ -15,13 +15,11 @@ void fill(matrix *mat, f32 x);
 void fill_rand(matrix *mat, f32 lower, f32 upper);
 void scale(matrix *mat, f32 scale);
 
-// I could just use operator overloading if in c++
-// but let it be whole C project
+// could've used operator overloading in c++
 b32 add(matrix *out, const matrix *a, const matrix *b);
 b32 sub(matrix *out, const matrix *a, const matrix *b);
-b32 matmul(matrix *out, const matrix *a, const matrix *b, b8 zero_ut,
-           b8 transpose_a,
-           b8 transpose_b); // matrix multiplication
+b32 matmul(matrix *out, const matrix *a, const matrix *b, b8 zero_out,
+           b8 transpose_a, b8 transpose_b); // matrix multiplication
 b32 relu(matrix *out, const matrix *in);
 b32 softmax(matrix *out, const matrix *in);
 b32 reinforce_loss(matrix *out, const matrix *probs, const matrix *advantages);
