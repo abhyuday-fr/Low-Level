cb6eb1b10417e911175f6c70d477cb54f515307b
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-06 21:38:24 +0530

    updated proper argument

--- ReinC/mat.h
+++ ReinC/mat.h
@@ -19,12 +19,13 @@ void scale(matrix *mat, f32 scale);
 // but let it be whole C project
 b32 add(matrix *out, const matrix *a, const matrix *b);
 b32 sub(matrix *out, const matrix *a, const matrix *b);
-b32 matmul(matrix *out, const matrix *a, const matrix *b, b8 transpose_a,
+b32 matmul(matrix *out, const matrix *a, const matrix *b, b8 zero_ut,
+           b8 transpose_a,
            b8 transpose_b); // matrix multiplication
 b32 relu(matrix *out, const matrix *in);
 b32 softmax(matrix *out, const matrix *in);
 b32 reinforce_loss(matrix *out, const matrix *probs, const matrix *advantages);
-b32 reinforce_add_grad(matrix *prob_grade, const matrix *probs,
+b32 reinforce_add_grad(matrix *prob_grads, const matrix *probs,
                        const matrix *rt, const matrix *grad);
 b32 relu_add_grad(matrix *out, const matrix *in, const matrix *grad);
 b32 softmax_add_grad(matrix *out, const matrix *softmax_out,
