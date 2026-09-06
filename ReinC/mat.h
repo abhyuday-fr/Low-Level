db0e98775f591aa0b660c4c09c3f88149ed5b0ab
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-06 20:35:25 +0530

    separated all matrix operations

---
+++ ReinC/mat.h
@@ -0,0 +1,33 @@
+#ifndef MAT_H
+#define MAT_H
+
+#include "arena.h"
+#include "base.h"
+
+typedef struct {
+  u32 rows, cols;
+  f32 *data; // row-major
+} matrix;
+
+matrix *create_matrix(mem_arena *arena, u32 rows, u32 cols);
+void clear(matrix *mat);
+void fill(matrix *mat, f32 x);
+void fill_rand(matrix *mat, f32 lower, f32 upper);
+void scale(matrix *mat, f32 scale);
+
+// I could just use operator overloading if in c++
+// but let it be whole C project
+b32 add(matrix *out, const matrix *a, const matrix *b);
+b32 sub(matrix *out, const matrix *a, const matrix *b);
+b32 matmul(matrix *out, const matrix *a, const matrix *b, b8 transpose_a,
+           b8 transpose_b); // matrix multiplication
+b32 relu(matrix *out, const matrix *in);
+b32 softmax(matrix *out, const matrix *in);
+b32 reinforce_loss(matrix *out, const matrix *probs, const matrix *advantages);
+b32 reinforce_add_grad(matrix *prob_grade, const matrix *probs,
+                       const matrix *rt, const matrix *grad);
+b32 relu_add_grad(matrix *out, const matrix *in, const matrix *grad);
+b32 softmax_add_grad(matrix *out, const matrix *softmax_out,
+                     const matrix *grad);
+
+#endif
