229514c3e0dcf0841ad219b5219ecd1ab4a9d9e7
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-18 22:46:54 +0530

    added driver (main.cc)

--- IPC-lockfree-ringbuffer/main.cc
+++ IPC-lockfree-ringbuffer/main.cc
@@ -1,9 +1,62 @@
+#include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <thread>
+
+#include "ipc_ringbuffer.hh"
+
+const std::string SHM_NAME = "/spsc_zero_copy_buffer";
+
+void run_producer() {
+  IpcRingBuffer ring_buffer;
+  if (!ring_buffer.init_as_prodcuer(SHM_NAME)) {
+    std::cerr << "Failed to init producer\n";
+    return;
+  }
+
+  std::cout << "[Producer] ready. Streaming data...\n";
+  uint8_t counter = 0;
+  while (true) {
+    if (ring_buffer.push(counter)) {
+      std::cout << "Sent: " << (int)counter << '\n';
+    } else {
+      std::this_thread::yield();
+    }
+  }
+}
+
+void run_consumer() {
+  IpcRingBuffer ring_buffer;
+  if (!ring_buffer.init_as_consumer(SHM_NAME)) {
+    std::cerr << "Failed to init consumer\n";
+    return;
+  }
+
+  std::cout << "[Consumer] attatched. Listening for data...\n";
+  uint8_t value;
+  while (true) {
+    if (ring_buffer.pop(value)) {
+      std::cout << "Received: " << (int)value << "\n";
+    } else {
+      std::this_thread::yield();
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << "[producer|consumer]\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string role = argv[1];
+
+  if (role == "producer") {
+    run_producer();
+  } else if (role == "consumer") {
+    run_consumer();
+  } else {
+    std::cerr << "Invalid argument\n";
     return EXIT_FAILURE;
   }
 
