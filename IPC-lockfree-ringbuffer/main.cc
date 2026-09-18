65188b441493dc76d17a86ee61c370b3d1bc2bc4
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-18 22:47:39 +0530

    updated correct usage message

--- IPC-lockfree-ringbuffer/main.cc
+++ IPC-lockfree-ringbuffer/main.cc
@@ -45,7 +45,7 @@ void run_consumer() {
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << "[producer|consumer]\n";
+    std::cerr << "Usage: " << argv[0] << " [producer|consumer]\n";
     return EXIT_FAILURE;
   }
 
