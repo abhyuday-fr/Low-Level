1f92e27064fdc8f826f6828576a8a44071cb1fcf
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-23 20:03:24 +0530

    added spin-backoff logic to avoid cpu thrashing

---
+++ IPC-lockfree-ringbuffer/backoff.cc
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) ||             \
+    defined(_M_IX86)
+#include <immintrin.h>
+#define CPU_PAUSE() _mm_pause()
+#elif defined(__aarch64__)
+#define CPU_PAUSE() __asm__ volatile("yield" ::: "memory")
+#else
+#define CPU_PAUSE()
+#endif
+
+class SpinBackoff {
+  int attempts = 0;
+
+public:
+  void spin() {
+    attempts++;
+
+    if (attempts < 10) {
+      CPU_PAUSE();
+    } else if (attempts < 20) {
+      std::this_thread::yield();
+    } else {
+      std::this_thread::sleep_for(std::chrono::microseconds(1));
+    }
+  }
+
+  void reset() { attempts = 0; }
+};
